81cc675849197e9b44e1b27f214c022541b5db4a
#LakshyaJangid <202351075@iiitvadodara.ac.in>
2026-02-09 02:19:46 +0530

    Implement CORS middleware for OPTIONS requests
    Added middleware to handle CORS preflight requests.

--- SERVER/server.js
+++ SERVER/server.js
@@ -36,7 +36,23 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,POST,PUT,DELETE,OPTIONS'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 
 
 app.use(express.json());
